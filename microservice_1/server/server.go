9dddebd8555e6a0f143fba49a3e46ed5f104cd92
Alex Punnen <alexcpn@gmail.com>
2022-08-17 15:41:15 +0530

    Changes to show in browser

--- microservice_1/server/server.go
+++ microservice_1/server/server.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	 log "github.com/sirupsen/logrus"
 	 pb "interfaces/microservice_1"
+	 "time"
 )
 
 // server is used to implement the server
@@ -14,9 +15,11 @@ type Server struct{
 func (s *Server)Search(ctx context.Context, in *pb.SearchRequest) (*pb.SearchResponse, error){
 	log.Printf("Received Emailid: %v", in.EmailId)
 	log.Printf("Received Query: %v", in.Query)
-	
+	var searchResponse = "Input received is: " + in.EmailId + "Query " + in.Query +"\n" + 
+	 "Response is: Some Valid Result at " + time.Now().Format(time.RFC850)
+	log.Printf("Sending Response: %v", searchResponse)
 	// Note this is the only place we use validate
-	response := pb.SearchResponse{SearchResponse: "Some Valid response from server " }
+	response := pb.SearchResponse{SearchResponse: searchResponse }
 	return &response, nil
 	
 }
--- microservice_1/ts_client/src/index.ts
+++ microservice_1/ts_client/src/index.ts
@@ -1,30 +1,58 @@
 import * as grpcWeb from 'grpc-web';
 
-import {SearchServiceClient} from '../generated/microservice_1/TestServiceClientPb';
-import {SearchRequest} from '../generated/microservice_1/test_pb';
-import {SearchResponse} from '../generated/microservice_1/test_pb';
+import { SearchServiceClient } from '../generated/microservice_1/TestServiceClientPb';
+import { SearchRequest } from '../generated/microservice_1/test_pb';
+import { SearchResponse } from '../generated/microservice_1/test_pb';
 
 
 
 const searchService = new SearchServiceClient('http://localhost:8080', null, null);
 
-const searchRequest = new SearchRequest();
-searchRequest.setQuery('query');
-searchRequest.setEmailId('my@emailId.com');
 
-const call = searchService.search(searchRequest, {}, (err: grpcWeb.RpcError, response:SearchResponse) => {
-  if (err) {
-    console.error(err);
-    return;
+// Some DOM Manipulation 
+
+window.onload = () => {
+  console.log('On Page Load');
+  let btn = document.getElementById("coolbutton");
+  var input = document.getElementById('showresulthere') as HTMLElement | null;
+  if (input != null) {
+    console.log(input.textContent);
+    btn.addEventListener("click", (e: Event) => showResult(input));
+  } else {
+    console.log("input is null");
   }
-  console.log(response.getSearchResponse());
-  console.log(response.getPageNumber());
-  console.log(response.getTotalPages());
+
 }
-);
-call.on('status', (status: grpcWeb.Status) => {
+
+function showResult(input: HTMLElement) {
+
+  //Call the Server with some hard coded values
+  const searchRequest = new SearchRequest();
+  searchRequest.setQuery('query');
+  searchRequest.setEmailId('my@emailId.com');
+  var temp_response = "";
+
+  const call = searchService.search(searchRequest, {}, (err: grpcWeb.RpcError, response: SearchResponse) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    temp_response = response.getSearchResponse();
+    console.log(temp_response);
+    if (input != null) {
+      input.textContent = temp_response;
+    } else {
+      console.log("input is null");
+    }
+  }
+  );
+
+  call.on('status', (status: grpcWeb.Status) => {
     if (status.metadata) {
       console.log('Received metadata');
-      console.log(status.metadata);
+      console.log("status.metadata", status.metadata);
     }
   });
+
+
+}
