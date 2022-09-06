73f464399dc9f8228d8db87ea4443bbf78488d70
Alex Punnen <alexcpn@gmail.com>
2022-09-06 17:18:32 +0530

    updated response

--- app_onboarding/server/server.go
+++ app_onboarding/server/server.go
@@ -13,10 +13,12 @@ type Server struct{
 }
 
 func (s *Server)OnBoardApp(ctx context.Context, in *pb.AppOnboardingRequest) (*pb.AppOnboardingResponse, error){
-	log.Printf("Received Emailid: %v", in.EmailId)
-	log.Printf("Received comanyname: %v", in.CompanyName)
-	var searchResponse = "Input received is: " + in.EmailId + "Query " + in.CompanyName +"\n" + 
-	 "Response is: Some Valid Result at " + time.Now().Format(time.RFC850)
+	log.Printf("Received Email-id: %v", in.EmailId)
+	log.Printf("Received Company Name: %v", in.CompanyName)
+	var searchResponse = "Application Updated \nCompany Name" + in.CompanyName +"\n" + 
+	"Application Name" + in.ApplicationName +"\n" + 
+	"Email ID" + in.EmailId +"\n" + 
+	 "at " + time.Now().Format(time.RFC850)
 	log.Printf("Sending Response: %v", searchResponse)
 	// Note this is the only place we use validate
 	response := pb.AppOnboardingResponse{AppResponse: searchResponse,CompanyId:12312,Timestamp:time.Now().Format(time.RFC850) }
